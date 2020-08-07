f6b5279b400aa7f119e05e696cca886f0b00a878
joseph <josephjohndemarco@gmail.com>
2020-08-06 22:32:33 -0400

    Add constants, state variables, init function, HTML and CSS skeleton.

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 /*----- constants -----*/
+const ROWS = 7;
+const COLS = 7;
+
+const overlord = {
+    'null': 'gray',
+    'flagged': 'yellow',
+    'safe': 'green',
+};
+
+
+
 /*----- app's state (variables) -----*/
+let winner;
+    /**
+     * winner states ---
+     * null
+     * won
+     * lost
+     */
+let board = [ [] ];
+
 /*----- cached element references -----*/
+const boardEl = document.getElementById('main-grid');
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
+init();
+
+function init() {
+    board = Array(ROWS).fill().map( () => Array(COLS).fill(null));
+    winner = null;
+
+    // renderBoard();
+}
+
+function renderBoard() {
+
+    // board.forEach(function(square, idx) {
+    //     squareEls[idx].style.background = colors[square];
+    // });
+}
