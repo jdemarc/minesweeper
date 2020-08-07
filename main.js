7245ad5e0607b9497e587d9c1f8a577444083d93
joseph <josephjohndemarco@gmail.com>
2020-08-07 18:40:04 -0400

    Periodic commit

--- main.js
+++ main.js
@@ -42,30 +42,38 @@ function init() {
 
 function renderBoard() {
 
+    buildBoard();
+    //layMines();
+}
+
+function buildBoard() {
     let idx = 0; // Variable to give each square a unique ID.
 
     for (let i = 0; i < ROWS; i++) {
-        let row = boardEl.insertRow(i);
-
+        let rowEl = document.createElement('tr');
+        
         for(let j = 0; j < COLS; j++) {
+            let squareEl = document.createElement('td');
+            
+            squareEl.classList.add('cell');
+            squareEl.setAttribute('id', idx);
+            squareEl.innerHTML = '';
 
-            let square = row.insertCell(j);
-            square.setAttribute('id', idx)
-            square.innerHTML = ' ';
+            rowEl.appendChild(squareEl);
             idx++;
         }
+        
+        boardEl.appendChild(rowEl);
     }
+    
 
-    // layMines();
 }
-
 function layMines() {
-
+    // Find a way to generate random location without repeating.
     for (let i = 0; i < MINE_COUNT; i++) {
         let randomMineIndex = Math.floor(Math.random() * (49 - 0 + 1) + 0);
 
-        //Assign to places on grid.
-    
+
         console.log(randomMineIndex);
     }
     
